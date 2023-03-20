1ed27d98c2875140994f75d336d36a97e856bcfc
Dhorajiya Nency <109361169+DhorajiyaNency07@users.noreply.github.com>
2023-03-20 11:37:06 +0530

    pyramid

---
+++ pyramid_star_pattern.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main(){
+    int i,j,k,rows;
+    
+    printf("Enter your number of Row:");
+    scanf("%d",&rows);
+    
+    int space=rows;
+    
+    for(i=1; i<=rows; i++)
+    {
+        for(j=1; j<=space; j++)
+        {
+            printf(" ");
+        }
+        for(k=1; k<=i; k++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+        space--;
+    }
+}
+
+
+// Enter your number of Row:5
+//       *
+//      * *
+//     * * *
+//    * * * *
+//   * * * * *
