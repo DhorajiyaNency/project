1c73db47a1c1852e7074f5807441ebd52e3acf5a
Dhorajiya Nency <109361169+DhorajiyaNency07@users.noreply.github.com>
2023-03-28 09:13:51 +0530

    28/03/2023

---
+++ pattern_using_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10][10];
+    int i, j;
+
+    // Fill the array with numbers 1 to 8 in each row
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            arr[i][j] = (j % 10) + 1;
+        }
+    }
+
+    // Print the array
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
