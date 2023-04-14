ffcd34e6cef79c3d758aea635dacea8e7661fcc7
Dhorajiya Nency <109361169+DhorajiyaNency07@users.noreply.github.com>
2023-04-14 14:34:06 +0530

    2023

--- pyramid_star_pattern.c
+++ pyramid_star_pattern.c
@@ -61,3 +61,65 @@ void main(){
 //         * * *
 //          * *
 //           *
+
+
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,rows=5;
+    for(i=1; i<=rows; i++)
+    {
+        for(j=1; j<=rows-i; j++)
+        {
+            printf("  ");
+        }
+        for(k=1; k<=i*2-1; k++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+//          *
+//        * * *
+//      * * * * *
+//    * * * * * * *
+//  * * * * * * * * *
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,rows=5;
+    for(i=rows; i>=1; i--)
+    {
+        for(j=1; j<=rows-i; j++)
+        {
+            printf("  ");
+        }
+        for(k=1; k<=i*2-1; k++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+//  * * * * * * * * *
+//    * * * * * * *
+//      * * * * *
+//        * * *
+//          *
